867bb577d15db113d11eebf070f71327bec89192
StevensCh10 <stevensch10@outlook.com>
2024-07-24 14:51:14 -0300

    Criacao do projeto frontend, React e Typescript

---
+++ Frontend/iManager/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+
+  return (
+    <div>
+      <Routes>
+        <Route path='/' />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
