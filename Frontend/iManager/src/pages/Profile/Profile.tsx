be4b5e2e9aef85c9bb92dd97f935b8d8a183b5f2
StevensCh10 <stevensch10@outlook.com>
2024-08-31 15:15:16 -0300

    Remocao de importacoes que nao estavam sendo usadas

--- Frontend/iManager/src/pages/Profile/Profile.tsx
+++ Frontend/iManager/src/pages/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth/AuthContext";
 import { User } from "../../types/User";
 import Navbar from "../../components/Navbar/Navbar";
